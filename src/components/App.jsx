6dc3e3cb19461ae3c5cbcc58bb64de0e7adae33b
Visaka Devi <visaka.chaves@consorcio.cl>
2021-04-10 16:13:49 -0400

    feat(prettier): prettier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import '../styles/components/app.scss'
-const App = () => {
-    return (
-        <div>
-            <h1>Hi</h1>
-        </div>
-    )
-}
+import React from 'react';
+import '../styles/components/app.scss';
+
+const App = () => (
+  <div>
+    <h1>Hi</h1>
+  </div>
+);
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,58 @@
-const path = require('path')
-const HtmlWebpackPlugin =  require('html-webpack-plugin')
-const MiniCssExtractPlugin  = require('mini-css-extract-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js', 'jsx']
-    },
-    module: {
-        rules: [
-            {
-                // Elementos que nos ayudan a detectar loaders, img, scss
-                test: /\.(js|jsx)$/,
-                exclude: /node-modules/,
-                use: {
-                    loader: 'babel-loader'
-                },
-
-            },
-            {
-                test: /\.html$/,
-                use: {
-                    loader: 'html-loader'
-                }
-            },
-            {
-                test: /\.(css|scss)$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    "css-loader",
-                    "sass-loader"
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            // como se envia a produccion
-            filename: './index.html'
-        }),
-        new MiniCssExtractPlugin ({
-            filename: 'assets/[name].css'
-        }),
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', 'jsx'],
+  },
+  module: {
+    rules: [
+      {
+        // Elementos que nos ayudan a detectar loaders, img, scss
+        test: /\.(js|jsx)$/,
+        exclude: /node-modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+        },
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
     ],
-    devServer: {
-        contentBase:  path.join(__dirname, 'dist'),
-        compress: true,
-        port: 3005,
-        open: true
-    }
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      // como se envia a produccion
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css',
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3005,
+    open: true,
+  },
+};
