96c115ca48239d1116ace8137ef42ea991f6e18d
Visaka Devi <visaka.chaves@consorcio.cl>
2021-04-11 01:03:42 -0400

    feat: helmet

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import initalState from '../initalState'
 import Products from './Products'
+import MetaHead from './Helmet'
 
-const Home =  () => (
+const Home = () => (
+              <>
+              <MetaHead
+                     title="Productos"
+                     description="Encuentra todos tus productos favoritos"
+                     image="https://davecast.s3.amazonaws.com/avatarnegativo.jpg"
+                     url="https://youthful-bassi-a35236.netlify.app/"
+              />
        <Products products={initalState.products} />
+       </>
 )
 
 export default Home
